b678135cc0f627768ee5298dbb70cd34abc0653b
Jonathan Hatch <38332640+jonathanhatch@users.noreply.github.com>
2020-01-21 09:30:55 -0600

    Create storage.sh

---
+++ storage.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt install nfs-kernel-server
+sudo mkdir -p /data
+sudo chown nobody:nogroup /data
+sudo chmod 777 /data
+sudo cat > /etc/exports <<EOF
+/data 192.168.2.0/24(rw,sync,no_subtree_check) 192.168.1.0/24(rw,sync,no_subtree_check)
+EOF
+sudo exportfs -a
+sudo systemctl restart nfs-kernel-server
